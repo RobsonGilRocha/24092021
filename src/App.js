4fecbe4b188b232291b0c1e9b5b466488af08ec1
Robson Gil Rocha <gil1990gil@hotmail.com>
2021-10-01 19:23:12 -0300

    s

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./index.css"
-import Home from "./components/Home"
+//import Home from "./components/Home"<Home/>
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Templete from "./components/Templete"
+import Templeto from "./components/Templete"
 import {Provider} from 'react-redux'
 import store from './store'
 
 function App() {
   return (
     <Provider store={store}>
-      <Templete>
-        <Header/>
-        <Home/>
-        <Footer/>
-      </Templete>
+    <>
+      <Header/>
+      <Templeto/>
+      <Footer/>
+    </>
     </Provider>
   );
 }
--- src/components/Templete/index.js
+++ src/components/Templete/index.js
@@ -1,6 +1,20 @@
-import styled from "styled-components";
+import {useSelector, useDispatch} from 'react-redux'
+import {Templente} from "./styles";
+import Home from '../Home';
+
+function Templento() {
+    const aluno = useSelector( state => state.aluno);
+
+  
+
+    return (
+      <Templente>
+          <ul>{aluno ? <Home/>:<h1>ALUNO_FALSE</h1>}</ul>
+          <ul>{aluno ? <h1>ALUNO_TRUE</h1>:<Home/>}</ul>
+       </Templente>
+    );
+  }
 
 
-const Templente = styled.div`
-`
-export default Templente
+  export default Templento;
+
